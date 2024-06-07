03d4e28d4b8e8eb9a31238f2f43a47368e826dd3
May <maythada.dku@mail.com>
2024-06-07 11:11:39 +0530

    add multiple_components

---
+++ multiple_components/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const App = () =>{
+  return(
+    <div>
+      <Header />
+      <main>
+        This is main section.
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ multiple_components/src/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Header = () =>{
+return(
+<header>This is header</header>
+);
+}
+
+export default Header;
