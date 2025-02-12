b83f374f24dff9b27649e83a5168fe4dc6b7ce58
May Thadar Kyaw <maythada.dku@gmail.com>
2025-02-12 22:16:37 +0530

    JSX Practice

---
+++ JSX-Practice/src/App.jsx
@@ -0,0 +1,12 @@
+//App.jsx
+import MyCom from './mycom.jsx';
+
+function App(){
+  return(
+    <>
+      <MyCom />
+    </>
+  );
+}
+
+export default App;
---
+++ JSX-Practice/src/mycom.jsx
@@ -0,0 +1,13 @@
+//mycom.jsx
+
+function MyCom(){
+    return(
+        <div className="class_name">
+            <h1>My name is MAY THADAR KYAW.</h1>
+            <h2>My hobby is watching movies.</h2>
+        </div>
+    );
+
+}
+
+export default MyCom;
