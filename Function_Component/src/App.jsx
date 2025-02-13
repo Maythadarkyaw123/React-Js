05b9d47dd0f0b566dc1b9350cc21e04fe5a01efe
May Thadar Kyaw <maythada.dku@gmail.com>
2025-02-13 16:08:25 +0530

    Functional Component Practice

---
+++ Function_Component/src/App.jsx
@@ -0,0 +1,13 @@
+import {Header,Footer} from './mycom.jsx';
+
+function App(){
+  return(
+    <>
+      <Header />
+      <h2>Hey This is the main content of website.</h2>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ Function_Component/src/mycom.jsx
@@ -0,0 +1,16 @@
+
+export function Header(){
+    return(
+        <>
+            <h1>This is header</h1>
+        </>
+    );
+}
+
+export function Footer(){
+    return(
+        <>
+            <h1>This is footer</h1>
+        </>
+    );
+}
