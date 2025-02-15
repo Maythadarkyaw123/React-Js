0258277fefd9e5d111d62d3e1f7929c248e0b7df
May Thadar Kyaw <maythada.dku@gmail.com>
2025-02-15 23:38:59 +0530

    update main.jsx

--- Profile-Card/src/main.jsx
+++ Profile-Card/src/main.jsx
@@ -6,5 +6,6 @@ import "./index.css";  // Optional for global styles
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <h1>Hello This is from Main.js</h1>
   </React.StrictMode>
 );
