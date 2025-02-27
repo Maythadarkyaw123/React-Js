45c893ad35e2c1fb942201386a5c13f9ae56bf05
May Thadar Kyaw <maythada.dku@gmail.com>
2025-02-27 17:57:38 +0530

    User Input Form

--- PROJECTS/User-input-form/src/UserInput.jsx
+++ PROJECTS/User-input-form/src/UserInput.jsx
@@ -2,6 +2,22 @@ import { useState } from "react";
 
 export function UserInput() {
   const [name, setName] = useState("");
+  const [age, setage] = useState(1);
+  const [student, setstudent] = useState(false);
+
+  const Myage = () =>{
+    setage(age+1)
+  }
+
+  const Myage1 = () =>{
+    if(age>1){     //stoping the age  value not less than 1
+        setage(age-1)
+    }
+  }
+
+  const MyStatus = () =>{
+    setstudent(!student)
+  }
 
   return (
     <>
@@ -11,8 +27,16 @@ export function UserInput() {
         id="myname" 
         value={name} 
         onChange={(e) => setName(e.target.value)} 
-      />
-      <button type="button" onClick={() => alert(`Welcome ${name}!`)}>Welcome msg</button>
+      /><br/> 
+
+      <button type="button" onClick={() => alert(`Welcome ${name}!`)}>Welcome msg</button> <br/>
+
+        <h2>Age: {age}</h2>
+        <button onClick={Myage}>Age + </button>
+        <button onClick={Myage1}>Age - </button> <br/>
+
+        <h2>Is Student? {student ? "Yes" : "No"}</h2> 
+        <button onClick={MyStatus}>Toggle</button> 
     </>
   );
 }
