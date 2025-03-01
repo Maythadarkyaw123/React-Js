40ea40c8d06a0a2435a5802a79eae05674be8b3d
May Thadar Kyaw <maythada.dku@gmail.com>
2025-03-01 14:50:27 +0530

    Fruit List

---
+++ Array-State/src/App.jsx
@@ -0,0 +1,9 @@
+import FruitList from "./FruitList";
+
+function App(){
+    return(
+      <FruitList/>
+    );
+}
+
+export default App;
---
+++ Array-State/src/FruitList.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function FruitList(){
+    const [fruits, setFruits] = useState(["apple","orange","grape"]);
+
+    function addNew(){
+        const newone = document.getElementById("newfruit").value;            //here be aware of value
+        document.getElementById("newfruit").value = "";                //after pressing add button text space will be clean
+    
+
+        setFruits(f => [...f, newone])
+    }
+
+    function delFruit(){
+        
+    }
+    
+
+    return(
+        <div>
+            <p>Fruits List: </p>
+            <ul>
+                {/* if we use "index" as key it can be rendering problem (not tracking the changes correctly) */}
+                {fruits.map((fruit,index) => 
+                    <li key={fruit}>
+                        {fruit}
+                        <button type="button" onClick={delFruit}>Delete</button>
+                    </li>
+                    
+                )} 
+        
+            </ul>
+
+            {/* <input type="text" name="newfruit" id="newfruit" />
+            <button type="button" onClick={addnewFruit}>Add</button> */}
+            <input type="text" name="newfruit" id="newfruit" />
+            <button type="button" onClick={addNew}>Add</button>
+        </div>
+    );
+
+}
+export default FruitList;
